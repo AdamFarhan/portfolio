9b7a8ba402d2263b6bbb30ea874065fc9cc98503
Adam Farhan <adamfarhan11@gmail.com>
2024-11-17 10:44:41 -0500

    Added scroll in animations for project list

--- src/slices/Projects/ProjectList/ProjectList.tsx
+++ src/slices/Projects/ProjectList/ProjectList.tsx
@@ -1,21 +1,61 @@
 "use client";
-import { asImageSrc, Content, isFilled } from "@prismicio/client";
-import { PrismicNextLink } from "@prismicio/next";
-import Image from "next/image";
+import { useRef } from "react";
 import Link from "next/link";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
 import { MdArrowOutward } from "react-icons/md";
+
+import { Content } from "@prismicio/client";
+import { useGSAP } from "@gsap/react";
+
 import { ProjectListItem } from "./ProjectListItem";
+
+gsap.registerPlugin(useGSAP, ScrollTrigger);
 
 type Props = {
   projects: Content.ProjectDocument[];
   fallbackItemImage: Content.ContentIndexSlice["primary"]["fallback_item_image"];
 };
 export const ProjectList = ({ projects, fallbackItemImage }: Props) => {
+  const projectsRef = useRef<Array<HTMLLIElement | null>>([]);
+  useGSAP(
+    () => {
+      projectsRef.current.forEach((project) => {
+        gsap.fromTo(
+          project,
+          {
+            opacity: 0,
+            y: 20,
+          },
+          {
+            opacity: 1,
+            y: 0,
+            duration: 1.3,
+            ease: "elastic.out(1, 0.3)",
+            scrollTrigger: {
+              trigger: project,
+              start: "top bottom-=100px",
+              end: "bottom center",
+              toggleActions: "play none none none",
+            },
+          }
+        );
+      });
+    },
+    { dependencies: [projectsRef] }
+  );
+
   return (
     <div>
       <ul className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-20">
-        {projects.map((project) => (
-          <li key={project.uid} className="list-item">
+        {projects.map((project, index) => (
+          <li
+            key={project.uid}
+            className="list-item"
+            ref={(element) => {
+              projectsRef.current[index] = element;
+            }}
+          >
             <ProjectListItem
               project={project}
               fallbackItemImage={fallbackItemImage}
